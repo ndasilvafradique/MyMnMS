a67f3cd8f9f128f4330487f92b4ac0d30ef90a09
idefalco <i.falco@studenti.unisannio.it>
2024-11-25 10:46:33 +0100

    fix replanning

--- src/mnms/simulation.py
+++ src/mnms/simulation.py
@@ -382,7 +382,7 @@ class Supervisor(object):
                 for u in users_to_replan:
                     # Interrupt user's path but keep user in the list of user_flow
                     u.interrupt_path(self.tcurrent)
-                self._decision_model.add_users_for_planning(list(users_to_replan), [Event.MATCH_FAILURE]*len(users_to_replan))
+                self._decision_model.add_users_for_planning(list(users_to_replan), [Event.INTERRUPTION]*len(users_to_replan))
 
                 if self._flow_motor._write:
                     self._flow_motor.write_result(affectation_step, flow_step, flow_dt)
