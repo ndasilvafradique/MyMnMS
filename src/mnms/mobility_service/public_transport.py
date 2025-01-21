434077aa7b47df63a384cbf34ba26fcdffdbc24c
assuntaDC <decaroassunta98@gmail.com>
2025-01-21 19:13:37 +0100

    Indentation fixing (again)

--- src/mnms/mobility_service/public_transport.py
+++ src/mnms/mobility_service/public_transport.py
@@ -530,8 +530,8 @@ class PublicTransportMobilityService(AbstractMobilityService):
                     if self._next_veh_departure[user_line_id] is None:
                         print(f'Next departure for user {user.id} on {user_line_id} is not available')
                         return Dt(hours=24)
-                departure_time, waiting_veh = self._next_veh_departure[user_line_id]
-                chosen_veh = waiting_veh
+                    departure_time, waiting_veh = self._next_veh_departure[user_line_id]
+                    chosen_veh = waiting_veh
 
         self._cache_request_vehicles[user.id] = (chosen_veh, chosen_line)
         print(f'Associate user {user.id} to {chosen_veh} {chosen_line}')
